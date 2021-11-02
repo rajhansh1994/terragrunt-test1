910b87c8d5bf5366e159c10d74af198bb51d6b97
Rajhansh <kumar.rajhansh@capgemini.com>
2021-11-02 17:49:14 +0530

    commenting

--- frontend-app/main.tf
+++ frontend-app/main.tf
@@ -12,10 +12,10 @@ terraform {
   }
 }
 
-resource "aws_instance" "example" {
+/*resource "aws_instance" "example" {
   # Run an Ubuntu 18.04 AMI on the EC2 instance.
   ami                    = "ami-0747bdcabd34c712a"
   instance_type          = "t2.micro"
  # vpc_security_group_ids = [aws_security_group.instance.id]
   key_name = "anz_demo"
-}
+}*/
